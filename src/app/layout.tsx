5d941f1fc3edb56b9f7d62b8b3377b27ca6167f7
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2023-12-05 19:11:59 -0300

    first-commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,21 @@
+import Link from "next/link";
 import type {Metadata} from "next";
 
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Restaurancy",
+  title: 'Restaurancy - Hello World',
+  description: 'The best restaurants in the world',
+  keywords: ['restaurant', 'food', 'eat', 'dinner', 'lunch'],
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
-        <header className="text-xl font-bold leading-[3rem]">Restaurancy</header>
+        <Link href='/' >
+          <header className="text-xl font-bold leading-[3rem]">Restaurancy</header>
+        </Link>
         <main className="py-8">{children}</main>
         <footer className="text-center leading-[3rem] opacity-70">
           © {new Date().getFullYear()} Restaurancy
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
-export default function HomePage() {
-  return <section>👋</section>;
+import api from "@/api";
+import Link from "next/link";
+import Image from "next/image";
+
+export default async function HomePage() {
+  const restaurants = await api.list();
+  
+  return (
+    <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3" >
+      {restaurants.map((restaurant)=>{
+        return(
+          <article key={restaurant.id} >
+            <img
+              alt={restaurant.name}
+              className="mb-3 h-[300px] w-full object-cover"
+              src={restaurant.image}
+            />
+            <h2 className="inline-flex gap-2 text-lg font-bold" >
+              <Link href={restaurant.id}>
+                <span>{restaurant.name}</span>
+              </Link>
+              <small>
+                <span>⭐</span>
+                <span>{restaurant.score}</span>
+                <span className="opacity-90" >{restaurant.ratings}</span>
+              </small>
+            </h2>
+            <p>{restaurant.description}</p>
+          </article>
+        )
+      })}
+    </section>
+  );
 }
